4279dfff0cc5106cbaa08a4b0161212ca65378e9
dxg76 <dantexgordon2034@gmail.com>
2025-03-23 13:43:19 -0500

    minor change

--- vending_logic_pi/menu-tree.cpp
+++ vending_logic_pi/menu-tree.cpp
@@ -14,17 +14,17 @@
 Menu_tree::Menu_tree(){
 
     //Creating the root node
-    root = new Node("Main Menu","",0,0,"wav files/Begin_Statement.wav");
+    root = new Node("Main Menu","",0,{},"wav files/Begin_Statement.wav");
 
     //Creating the Candy nodes
-    Node* candy = new Node("candy","",0,0,"wav files/what_candy.wav");
+    Node* candy = new Node("candy","",0,{},"wav files/what_candy.wav");
         candy->add_child(new Node("kitkat", "E7", 1.00, {4,4}, "wav files/KitKat_Statement.wav")); 
         candy->add_child(new Node("reese's", "E1", 1.00, {4,4}, "wav files/Reeses_Statement.wav"));
         candy->add_child(new Node("butterfinger", "E3", 1.00, {4,4}, "wav files/Butter_Finger_Statement.wav"));
         candy->add_child(new Node("crunch", "E5", 1.00, {10,10,10,10}, "wav files/Musketeers_Statement.wav"));
 
     //Creating the Chip nodes
-    Node* chips = new Node("chips","",0,0,"wav files/what_chips.wav");
+    Node* chips = new Node("chips","",0,{},"wav files/what_chips.wav");
         chips->add_child(new Node("doritos", "A1", 1.00, {10,10,10,10}, "wav files/Doritos_Statement.wav"));
         chips->add_child(new Node("cheetos", "B1", 1.00, {10,10,10,10}, "wav files/Cheetos_Statement.wav"));
         chips->add_child(new Node("pretzels", "D1", 1.00, {10,10,10,10}, "wav files/Pretzels_Statement.wav"));
