bf8a11bd4bfd728c4a77d1cb360f8ebdfe37a6c5
dxg76 <dantexgordon2034gmail.com>
2025-03-26 19:05:11 +0000

    vendor.mk

--- vending_logic_pi/vendor.cpp
+++ vending_logic_pi/vendor.cpp
@@ -8,8 +8,8 @@ Vendor::Vendor(bool mode, bool voice_control, bool no_charge){
     confirmation_prompt = false;
     configure_all();
     set_debug(mode);
-    this->voice_control = voice_control;
-    this->no_charge = no_charge;
+    set_voice_control(voice_control);
+    set_no_charge(no_charge);
 }
 
 //vendor methods
@@ -30,7 +30,7 @@ void Vendor::set_voice_control(bool voice_control){
 void Vendor::set_no_charge(bool no_charge){
     this->no_charge = no_charge;
     if(no_charge){
-        std::cout<<"NO CHARGE ACTIVE MODE" << std::endl;
+        std::cout<<"NO CHARGE MODE ACTIVE" << std::endl;
     }
 }
 
@@ -549,7 +549,7 @@ bool Vendor::try_payment(float item_cost){
                 //total_currency += check_coins();
                 card_payment = check_card_payment(item_cost);
             }
-        }            
+        }
         std::cout << "payment complete!" << std::endl;
         list_menu = true;
         state = 3;
