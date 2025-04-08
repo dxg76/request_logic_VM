f17b1d6efe9ef3fa1d6ed78fe06b65b43ea7fc17
dxg76 <dxg2@students.uwf.edu>
2025-04-08 14:17:48 -0500

    mes

--- vending_logic_pi/vendor.cpp
+++ vending_logic_pi/vendor.cpp
@@ -121,7 +121,7 @@ float Vendor::read_hex_code(std::string hex_code){
         return accept_coins(hex);
     }
     //bill detected
-    return accept_bills(hex);s
+    return accept_bills(hex);
 }
 
 std::string Vendor::generate_prompt(Node* current_node){
