f6cd434acca0b354011027ed685d9030272eaa0b
Devanm611 <dmr76@students.uwf.edu>
2025-04-14 13:59:18 -0500

    invalid selection wav

--- vending_logic_pi/main.cpp
+++ vending_logic_pi/main.cpp
@@ -343,8 +343,9 @@ int main(int argc, const char** argv){
             play_wav_file("wav files/select_skip.wav");
             vendor.state = 1;
             vendor.list_menu = false;
+            vendor.print_tokens();
             current_node = current_node->find_child(vendor_result);
-            vendor_result =vendor.check_inventory(current_node);
+            vendor_result = vendor.check_inventory(current_node);
             if(vendor_result == "err"){
                 play_wav_file("wav files/invalid_selection.wav"); //DEV NOTE make this audio file
                 play_wav_file("wav files/return_home.wav");
